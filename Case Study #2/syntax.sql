fba41866b6d84a1d0ff8720c2a166b7448bad7b9
Akia <103283421+Akia14@users.noreply.github.com>
2022-05-18 17:12:20 -0500

    Update syntax.sql

--- Case Study #2/syntax.sql
+++ Case Study #2/syntax.sql
@@ -7,7 +7,7 @@
 ## Let's Investigate The Tables First
 
 SELECT * FROM pizza_runner.runners
---There are 4 runners and this tables shows their registration date. Nothing to clean 
+--There are 4 runners and this tables shows their registration date. Nothing to clean.
 
 SELECT * FROM pizza_runner.customer_orders;
 --Exclusions, extras, and order_time column must be cleaned 
@@ -19,7 +19,7 @@ SELECT * FROM pizza_runner.pizza_names;
 --Only 2 names for ``pizza_id` meat lovers & vegetarians 
 
 SELECT * FROM pizza_runner.pizza_recipes;
---shows the standar set of toppings for each pizza_names
+--shows the standard set of toppings for each pizza_names
 
 SELECT * FROM pizza_runner.pizza_toppings; 
 --Shows the topping names relevant to the ``topping_id``
@@ -31,3 +31,10 @@ SELECT * FROM pizza_runner.pizza_toppings;
 
 SELECT COUNT(*) FROM pizza_runner.customer_orders;
 
+-- 2. How many unique customer orders were made?
+
+SELECT COUNT(DISTINCT(order_id)) AS unique_customer_order FROM pizza_runner.customer_orders;
+
+--3. How many successful orders were delivered by each runner?
+
+SELECT * FROM pizza_runner.runner_orders;
